769a170f6465d76fdf6283471febfcf1ce113578
Chris Prather <chris.prather@reopenlabs.com>
2023-01-21 19:08:50 +0000

    pull in JWT signer and make more stuff configurable by env variables

--- prod.secret.exs
+++ prod.secret.exs
@@ -1,8 +1,10 @@
 import Mix.Config
 
 config :pleroma, Pleroma.Web.Endpoint,
-  url: [host: "ack.nerdfight.online"]
+  url: [host: System.get_env("DOMAIN")]
 
-config :pleroma, Pleroma.Web.WebFinger, domain: "nerdfight.online"
+config :pleroma, Pleroma.Web.WebFinger, domain: System.get_env("STATIC_DOMAIN")
+
+config :joken, default_signer: System.get_env("JWT_SIGNER")
 
 config :pleroma, configurable_from_database: true
