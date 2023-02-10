2bd96ced2511f0479218b187b17fd1c937661b37
Chris Prather <chris.prather@reopenlabs.com>
2023-02-10 01:28:58 +0000

    update back to all perl

--- mills_lane/fnff.pl
+++ mills_lane/fnff.pl
@@ -2,9 +2,14 @@
 use 5.34.0;
 use local::lib q(local);
 
-use Mastodon::Client;
 use DBI;
 use DBD::Pg qw(:pg_types);
+use Getopt::Long;
+use Mastodon::Client;
+
+GetOptions(
+    "dry-run|n" => \my $dry_run,
+);
 
 my $dbh = DBI->connect("dbi:Pg:host=$ENV{DB_HOST};dbname=$ENV{DB_NAME}", $ENV{DB_USER}, $ENV{DB_PASS}, {AutoCommit => 0});
 
@@ -23,6 +28,8 @@ my $sql = q{
     LIMIT 10
 };
 
+say STDERR $sql;
+
 my $winners = $dbh->selectcol_arrayref($sql);
 $dbh->disconnect;
 
@@ -31,9 +38,29 @@ my $list = join "\n", map qq{\@$_}, $winners->@*;
 my $post = qq{
 #FF #FakeNerdFightFriday
 
+As \@fitzgepn\@mas.to explained on <a
+href="https://twitter.com/fitzgepn/status/1297712297595400192">the birdsite</a>:
+
+\@jacobydave\@mastodon.xyz has written about his #ff script and the curious
+phenomena of #fakeNerdFightFriday that sprang up from it <a
+href="https://jacoby.github.io/2019/07/05/the-social-experiment-of-followfriday.html">here</a>.
+
+Here's this week's winners:
+
 $list
 
 LET'S GET IT ON!
 };
 
-say $post;
+my $client = Mastodon::Client->new(
+    instance => $ENV{INSTANCE},
+    name => $ENV{USERNAME},
+    client_id => $ENV{CLIENT_ID},
+    client_secret => $ENV{CLIENT_SECRET},
+    access_token => $ENV{ACCESS_TOKEN},,
+    coerce_entites => 1,
+);
+
+say STDERR $post;
+
+#$client->post_status($post) unless $dry_run;
