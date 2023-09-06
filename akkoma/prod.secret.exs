277a0e6c5c01020f2394c593b47f363d8e4ee9ff
Chris Prather <chris.prather@reopenlabs.com>
2023-09-05 23:07:07 -0400

    add "force_custom_plan" config for DB performance

--- akkoma/prod.secret.exs
+++ akkoma/prod.secret.exs
@@ -7,4 +7,10 @@ config :pleroma, Pleroma.Web.WebFinger, domain: System.get_env("STATIC_DOMAIN")
 
 config :joken, default_signer: System.get_env("JWT_SIGNER")
 
+config :pleroma, Pleroma.Repo,
+  prepare: :named,
+  parameters: [
+    plan_cache_mode: "force_custom_plan"
+  ]
+
 config :pleroma, configurable_from_database: true
