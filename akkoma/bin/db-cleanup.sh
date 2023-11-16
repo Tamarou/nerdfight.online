cc021553a9980b7bb33cac0ff57789c849976d58
Chris Prather <chris.prather@reopenlabs.com>
2023-11-16 11:58:08 -0500

    make db-cleanup more vocal

--- akkoma/bin/db-cleanup.sh
+++ akkoma/bin/db-cleanup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh +x
+set -e
 
-# prune old objects from the DB
-/opt/akkoma/bin/pleroma_ctl database prune_objects --prune-orphaned-activities --keep-threads
+export HOME=/opt/akkoma
 
-# prune stale things from the DB
-/opt/akkoma/bin/pleroma_ctl database prune_task
+while ! pg_isready -U "${DB_USER:-akkoma}" -d "postgres://${DB_HOST:-db}:${DB_PORT:-5432}/${DB_NAME:-akkoma}" -t 1; do
+    sleep 1s
+done
+
+echo "-- prune old objects from the DB --"
+"$HOME"/bin/pleroma_ctl database prune_objects --prune-orphaned-activities --keep-threads
+
+echo "-- prune stale things from the DB --"
+"$HOME"/bin/pleroma_ctl database prune_task
